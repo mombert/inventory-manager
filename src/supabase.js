f091e02f83854a6fe3b37ecbc842e821244325c2
mombert <34613324+mombert@users.noreply.github.com>
2026-08-04 10:54:06 -0400

    Delete src/supabase.js

--- src/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const url = process.env.REACT_APP_SUPABASE_URL;
-const key = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(url, key);
-export const configured = Boolean(url && key);
